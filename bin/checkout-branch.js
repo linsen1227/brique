f2912ee08e228314f4e66aade40a8a506cb5ae9f
linsen <linsen@youzan.com>
2022-03-08 16:47:32 +0800

    feat: 添加gcb指令

--- bin/checkout-branch.js
+++ bin/checkout-branch.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
-
-const { exec } = require('child_process');
 const program = require('commander');
 const inquirer = require('inquirer');
 const shell = require('shelljs');
+
+function generateDate () {
+  const dateTime = new Date();
+  const year = dateTime.getFullYear();
+  const month = dateTime.getMonth() + 1;
+  const date = dateTime.getDate();
+
+  function appendZero (num) {
+    if (num < 10) return '0' + num;
+    return num;
+  }
+
+  return appendZero(year) + appendZero(month) + appendZero(date);
+}
 
 inquirer.prompt([{
   type: 'list',
@@ -16,10 +28,31 @@ inquirer.prompt([{
 }]).then(async function(result) {
   const { branchType } = result;
   
-  const currentBranch = shell.exec('git symbolic-ref --short -q HEAD').stdout;
+  const currentBranch = shell.exec('git rev-parse --abbrev-ref HEAD').stdout;
 
-  if (currentBranch !== 'master') {
+  if (currentBranch.trim() !== 'master') {
     shell.echo('Error: 请从master下拉分支');
     shell.exit(1);
   }
+
+  const inputValue = await inquirer.prompt([{
+    type: 'input',
+    message: '请输入分支名',
+    name: 'branchName',
+    validate: function (val) {
+      const done = this.async();
+      if (!val) {
+        done('分支名必填');
+        return
+      }
+      done(null, true);
+    }
+  }])
+  const { branchName } = inputValue;
+
+  shell.exec('git pull');
+
+  const username = shell.exec('git config user.name').stdout;
+
+  shell.exec(`git checkout -b ${branchType}/${username}-${branchName}-${generateDate()}`);
 });
