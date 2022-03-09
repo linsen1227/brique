bf3586dac8204408458b6d942a00c0dca54ae14b
linsen <linsen@youzan.com>
2022-03-09 10:58:23 +0800

    feat: 添加release脚本

--- bin/checkout-branch.js
+++ bin/checkout-branch.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-const program = require('commander');
 const inquirer = require('inquirer');
 const shell = require('shelljs');
 
---
+++ bin/release.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const inquirer = require('inquirer');
+const shell = require('shelljs');
+
+function trim (str) {
+  return str.replace(/(\r\n\t|\n|\r\t)/g, "");
+}
+
+const currentBranch = shell.exec('git rev-parse --abbrev-ref HEAD').stdout;
+
+// if (trim(currentBranch) !== 'master') {
+//   shell.echo('Error: 请先切到master分支');
+//   shell.exit(1);
+// }
+
+shell.exec('git pull');
+
+const branchList = shell.exec('git branch').stdout.split('\n').map((branch) => {
+  return branch.replace(/[*|\s]/g, '');
+}).filter((branch) => {
+  if (branch) return branch;
+});
+
+inquirer.prompt([{
+  type: 'list',
+  message: '请选择要合并的分支',
+  name: 'branch',
+  choices: branchList
+}]).then((result) => {
+  const { branch } = result;
+
+  shell.exec(`git merge ${branch}`);
+})
